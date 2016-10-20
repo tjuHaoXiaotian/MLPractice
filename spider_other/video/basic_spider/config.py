295a3e94b3cd99cb52cecdf6ff75be1381b92ac2
tjuHaoXiaotian <15122938076@163.com>
2016-10-20 19:15:31 +0800

    不记录config

--- spider_other/video/basic_spider/config.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-
-# root_url = "http://www.pppav7171.net/forum-58-1.html" #偷拍自拍
-# name = '偷拍自拍-偷偷撸影院'
-
-root_url = "http://www.pppav7171.net/forum-62-1.html"  # 欧美性爱
-name = '欧美性爱-偷偷撸影院'
